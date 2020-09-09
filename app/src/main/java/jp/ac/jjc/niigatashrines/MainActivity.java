2827937570ae5d4bce58e56b202872ba9b8d65db
Kosuke Fujiwara <solo.wing.pixy.adfx01@gmail.com>
2020-09-09 12:23:15 +0900

    修正２（20200909）

--- app/src/main/java/jp/ac/jjc/niigatashrines/MainActivity.java
+++ app/src/main/java/jp/ac/jjc/niigatashrines/MainActivity.java
@@ -2,7 +2,11 @@ package jp.ac.jjc.niigatashrines;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButtonListener listener = new ButtonListener();
+
+        Button JoetsuB = findViewById(R.id.btnJoetsu);
+        JoetsuB.setOnClickListener(listener);
+        Button ChuetsuB = findViewById(R.id.btnChuetsu);
+        ChuetsuB.setOnClickListener(listener);
+        Button KaetsuB = findViewById(R.id.btnKaetsu);
+        KaetsuB.setOnClickListener(listener);
+
+        Button MannersB = findViewById(R.id.btnManners);
+        MannersB.setOnClickListener(listener);
+    }
+
+    private class ButtonListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            Intent intent = getIntent();
+            int Id = view.getId();
+            switch (Id){
+                case R.id.btnJoetsu:
+                    intent = new Intent(MainActivity.this, JoetsuActivity.class);
+                    startActivity(intent);
+                case R.id.btnChuetsu:
+                    intent = new Intent(MainActivity.this, ChuetsuActivity.class);
+                    startActivity(intent);
+                case R.id.btnKaetsu:
+                    intent = new Intent(MainActivity.this, KaetsuActivity.class);
+                    startActivity(intent);
+                case R.id.btnManners:
+                    intent = new Intent(MainActivity.this, MannersActivity.class);
+                    startActivity(intent);
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/jp/ac/jjc/niigatashrines/MannersActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.jjc.niigatashrines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MannersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manners);
+    }
+}
